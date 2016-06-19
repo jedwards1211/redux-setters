8c3610ac8ae4c36fc8b9fbb5f3d11f726cd2dda6
Andy Edwards <jedwards@fastmail.com>
2016-06-19 17:29:51 -0500

    implement more features and write tests

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import snakeCase from 'lodash.snakecase'
 import camelCase from 'lodash.camelcase'
 import mapValues from 'lodash.mapvalues'
 import keyBy from 'lodash.keyby'
+import last from 'lodash.last'
 
 type Action = {
   type: string,
@@ -16,20 +17,18 @@ type Setter = {
   sub: (path: any[]) => Setter
 }
 
+function normalize(elem: any) {
+  // flow-issue(redux-setters)
+  return typeof elem === 'symbol' ? elem.substring(7) : elem
+}
+
 export function createSetter(path: any[], options?: {
-  basePath?: any[],
   domain?: string,
 } = {}): Setter {
-  const {basePath, domain} = options
-
-  let type = 'SET_' + path.filter(p => typeof p !== 'number')
-    // flow-issue(redux-setters)
-      .map(p => typeof p === 'symbol'
-        ? snakeCase(p).substring(7)
-        : snakeCase(p))
-      .join('.')
-      .toUpperCase()
-  if (domain) type = domain + type
+  const {domain} = options
+  
+  let type = 'SET_' + snakeCase(normalize(last(path))).toUpperCase()
+  if (domain) type = domain + '.' + type
 
   function set(payload: any): Action {
     const action: Action = {
@@ -37,7 +36,7 @@ export function createSetter(path: any[], options?: {
       payload,
       meta: {
         setter: true,
-        reduxPath: basePath ? [...basePath, ...path] : path
+        reduxPath: path
       }
     }
 
@@ -49,24 +48,28 @@ export function createSetter(path: any[], options?: {
   set.sub = (subpath, options = {}) => {
     let suboptions = {
       domain: domain || '',
-      basePath: path
     }
     if (options.domain) suboptions.domain += '.' + options.domain
     return createSetter([...path, ...subpath], suboptions)
+  }
+  set.subs = (subfields, options = {}) => {
+    let suboptions = {
+      domain: domain || '',
+    }
+    if (options.domain) suboptions.domain += '.' + options.domain
+    if (Array.isArray(subfields)) subfields = subfields.map(subpath => [...path, ...subpath])
+    else subfields = mapValues(subfields, subpath => [...path, ...subpath])
+    return createSetters(subfields, suboptions)     
   }
 
   return set
 }
 
 export function createSetters(fields: Array<any[]> | {[name: string]: any[]}, options?: {
-  basePath?: any[],
   domain?: string,
 } = {}): {[name: string]: Setter} {
   if (Array.isArray(fields)) {
-    fields = keyBy(fields, path => camelCase('set' + path.filter(p => typeof p !== 'number')
-      // flow-issue(redux-setters)
-      .map(p => typeof p === 'symbol' ? p.substring(7) : p)
-      .join(' ')))
+    fields = keyBy(fields, path => camelCase('set ' + normalize(last(path))))
   }
 
   return mapValues(fields, (path, name) => createSetter(path, options))
---
+++ test/index.js
@@ -0,0 +1,87 @@
+import {createSetter} from '../src'
+
+import {expect} from 'chai'
+
+describe('createSetter', () => {
+  describe('without options', () => {
+    describe('with single path element', () => {
+      it('creates action with correct properties', () => {
+        const setName = createSetter(['name'])
+        const action = setName('Andy')
+        expect(action.type).to.equal('SET_NAME')
+        expect(action.payload).to.equal('Andy')
+        expect(action.error).to.equal(undefined)
+        expect(action.meta.reduxPath).to.deep.equal(['name'])
+
+        const action2 = setName(new Error('test'))
+        expect(action2.error).to.equal(true)
+      })
+    })
+    describe('with multiple path elements', () => {
+      it('creates action with correct properties', () => {
+        const setName = createSetter(['user', 'name'])
+        const action = setName('Andy')
+        expect(action.type).to.equal('SET_NAME')
+        expect(action.payload).to.equal('Andy')
+        expect(action.error).to.equal(undefined)
+        expect(action.meta.reduxPath).to.deep.equal(['user', 'name'])
+      })
+    })
+  }) 
+  describe('with domain', () => {
+    describe('with single path element', () => {
+      it('creates action with correct properties', () => {
+        const setName = createSetter(['name'], {domain: 'ACCOUNTS'})
+        const action = setName('Andy')
+        expect(action.type).to.equal('ACCOUNTS.SET_NAME')
+        expect(action.payload).to.equal('Andy')
+        expect(action.error).to.equal(undefined)
+        expect(action.meta.reduxPath).to.deep.equal(['name'])
+
+        const action2 = setName(new Error('test'))
+        expect(action2.error).to.equal(true)
+      })
+    })
+    describe('with multiple path elements', () => {
+      it('creates action with correct properties', () => {
+        const setName = createSetter(['user', 'name'], {domain: 'ACCOUNTS'})
+        const action = setName('Andy')
+        expect(action.type).to.equal('ACCOUNTS.SET_NAME')
+        expect(action.payload).to.equal('Andy')
+        expect(action.error).to.equal(undefined)
+        expect(action.meta.reduxPath).to.deep.equal(['user', 'name'])
+      })
+    })    
+  })
+  describe('sub', () => {
+    it('creates action creator that creates actions with correct properties', () => {
+      const setName = createSetter(['UserView', 'document'], {domain: 'USER_VIEW'})
+        .sub(['name'], {domain: 'USER'})
+      
+      const action = setName('Andy')
+      expect(action.type).to.equal('USER_VIEW.USER.SET_NAME')
+      expect(action.payload).to.equal('Andy')
+      expect(action.error).to.equal(undefined)
+      expect(action.meta.reduxPath).to.deep.equal(['UserView', 'document', 'name'])
+    })
+  })
+  describe('subs', () => {
+    it('works', () => {
+      const {setFirstName, setLastName} = createSetter(['UserView', 'document'], {domain: 'USER_VIEW'})
+        .subs([['firstName'], ['lastName']], {domain: 'USER'})
+      let action
+      
+      action = setFirstName('Andy')
+      expect(action.type).to.equal('USER_VIEW.USER.SET_FIRST_NAME')
+      expect(action.payload).to.equal('Andy')
+      expect(action.error).to.equal(undefined)
+      expect(action.meta.reduxPath).to.deep.equal(['UserView', 'document', 'firstName'])
+      
+      action = setLastName('Edwards')
+      expect(action.type).to.equal('USER_VIEW.USER.SET_LAST_NAME')
+      expect(action.payload).to.equal('Edwards')
+      expect(action.error).to.equal(undefined)
+      expect(action.meta.reduxPath).to.deep.equal(['UserView', 'document', 'lastName'])
+    })
+  })
+})
